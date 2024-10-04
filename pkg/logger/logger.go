b1c5182b416cec85d38903d79114afa7a88b25cd
Kadir Belkuyu <kadirbelkuyu@gmail.com>
2024-10-05 01:56:54 +0300

    Update actions/checkout to v4.2.0 and add system-metrics artifact in CI (#7)
    * feat: implement pagination and add GitHub Actions CI/CD pipeline (#3)
    
    - Added pagination to handle large data sets efficiently.
    - Integrated GitHub Actions for continuous integration and deployment.
    
    * Update build.yml
    
    * Update build.yml
    
    * fix(ci): ensure public directory exists in pipeline
    
    * fix(ci): ensure public directory contains files before uploading artifact (#4)
    
    * Update build.yml
    
    * fix(ci): artifact (#5)
    
    * chore(ci): update actions/checkout to v4.2.0 (#6)

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"fmt"
+	"log"
 	"systemMetric/internal/domain"
 	"systemMetric/internal/repository"
 )
@@ -8,10 +10,15 @@ import (
 type Logger interface {
 	LogMetrics(metrics *domain.SystemMetrics) error
 	GetAllLogs() ([]domain.SystemMetrics, error)
+	GetLogs(offset, limit int) ([]domain.SystemMetrics, error)
 }
 
 type logger struct {
 	repo *repository.PostgresRepository
+}
+
+func LogSystemMetrics(metrics domain.SystemMetrics) {
+	fmt.Printf("System Metrics: %+v\n", metrics)
 }
 
 func NewLogger(repo *repository.PostgresRepository) Logger {
@@ -24,4 +31,33 @@ func (l *logger) LogMetrics(metrics *domain.SystemMetrics) error {
 
 func (l *logger) GetAllLogs() ([]domain.SystemMetrics, error) {
 	return l.repo.GetAllLogs()
+}
+
+func (l *logger) GetLogs(offset, limit int) ([]domain.SystemMetrics, error) {
+	query := `SELECT created_at, cpu_usage, memory_usage, disk_usage FROM logs ORDER BY created_at DESC LIMIT $1 OFFSET $2`
+	rows, err := l.repo.DB.Query(query, limit, offset)
+	if err != nil {
+		log.Printf("Error querying logs: %v", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	var logs []domain.SystemMetrics
+	for rows.Next() {
+		var metric domain.SystemMetrics
+		if err := rows.Scan(&metric.CreatedAt, &metric.CPUUsage, &metric.MemoryUsage, &metric.DiskUsage); err != nil {
+			log.Printf("Error scanning log: %v", err)
+			fmt.Printf("Scanned metric: %+v\n", metric)
+			return nil, err
+		}
+		logs = append(logs, metric)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("Error iterating rows: %v", err)
+		return nil, err
+	}
+
+	log.Printf("Fetched logs from DB: %v", logs)
+	return logs, nil
 }
--- scripts/init.sql
+++ scripts/init.sql
@@ -5,3 +5,10 @@ CREATE TABLE system_logs (
      disk_usage FLOAT NOT NULL,
      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+CREATE TABLE logs (
+      id SERIAL PRIMARY KEY,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      cpu_usage FLOAT NOT NULL,
+      memory_usage FLOAT NOT NULL,
+      disk_usage FLOAT NOT NULL
+);
